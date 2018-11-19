893ac6afe8dcf82f67a7a61240ff0fb902bc5d8b
outprog <xiongwei_13@hotmail.com>
2018-11-19 15:21:20 +0800

    uitls: fixed GetTransactionHash params name

--- utils/transaction_hash.go
+++ utils/transaction_hash.go
@@ -8,7 +8,7 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
-func GetTransactionHash(salt *big.Int, signerAddress common.Address, data []byte, exchangeAddress common.Address) []byte {
+func GetTransactionHash(salt *big.Int, takerAddress common.Address, data []byte, exchangeAddress common.Address) []byte {
 	domainHash := crypto.Keccak256(EIP712_DOMAIN_SEPARATOR_SCHEMA_HASH,
 		crypto.Keccak256([]byte("0x Protocol")),
 		crypto.Keccak256([]byte("2")),
@@ -16,13 +16,13 @@ func GetTransactionHash(salt *big.Int, signerAddress common.Address, data []byte
 	)
 	return crypto.Keccak256([]byte(EIP191_HEADER),
 		domainHash,
-		hashTransaction(salt, signerAddress, data, exchangeAddress))
+		hashTransaction(salt, takerAddress, data, exchangeAddress))
 }
 
-func hashTransaction(salt *big.Int, signerAddress common.Address, data []byte, exchangeAddress common.Address) []byte {
+func hashTransaction(salt *big.Int, takerAddress common.Address, data []byte, exchangeAddress common.Address) []byte {
 	return crypto.Keccak256(EIP712_ZEROEX_TRANSACTION_SCHEMA_HASH,
 		math.PaddedBigBytes(salt, 32),
-		addressToByte32(signerAddress),
+		addressToByte32(takerAddress),
 		crypto.Keccak256(data),
 	)
 }
