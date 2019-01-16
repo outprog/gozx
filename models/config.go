51cdd929a17bac2d6616005955b2e2c2d78f8dc5
outprog <xiongwei_13@hotmail.com>
2019-01-17 01:06:49 +0800

    updated kovan address

--- models/config.go
+++ models/config.go
@@ -26,9 +26,9 @@ var (
 	KovanConfig = Config{
 		EthRPC: "https://kovan.infura.io",
 
-		Erc20ProxyContractAddress:  common.HexToAddress("0x9ad1b8209cea603892c9dfaa676bc737088b499a"),
-		Erc721ProxyContractAddress: common.HexToAddress("0xbb428f7108971ecf1144700c4d37792f8b74f6b0"),
-		ExchangeContractAddress:    common.HexToAddress("0xa458ec0709468996ef2ef668f5e52f37ceb66627"),
+		Erc20ProxyContractAddress:  common.HexToAddress("0xf1ec01d6236d3cd881a0bf0130ea25fe4234003e"),
+		Erc721ProxyContractAddress: common.HexToAddress("0x2a9127c745688a165106c11cd4d647d2220af821"),
+		ExchangeContractAddress:    common.HexToAddress("0x35dd2932454449b14cee11a94d3674a936d5d7b2"),
 
 		WethAddress: KovanTokens["WETH"].Address,
 	}
